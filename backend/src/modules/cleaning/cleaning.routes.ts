086ff7329ba9090a59d755e80193960b92ad02d2
bichen4403-netizen <bi.chen4403@gmail.com>
2026-05-11 12:54:38 +0200

    fix: remove unused cleaning module and related files

--- backend/src/modules/cleaning/cleaning.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express"
-import cleaningController from "./cleaning.controller"
-
-const router = Router()
-
-router.get("/", cleaningController.getState)
-router.put("/", cleaningController.saveState)
-
-export default router
--- backend/src/modules/cleaning/types/cleaning.entity.ts
+++
@@ -1,6 +0,0 @@
-export default interface CleaningEntity {
-	section_id: number
-	duty_template: string
-	days_without_cleaning: string
-	schedule: string
-}
