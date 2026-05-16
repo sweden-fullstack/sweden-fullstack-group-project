c6c176d5f13db47f9386aa44e37b362b53d5fefe
d-najd <dimitar.hristijan.najdovski@gmail.com>
2026-05-16 18:15:01 +0200

    remove dead code

--- backend/src/errors/NotImplementedError.ts
+++
@@ -1,8 +0,0 @@
-import AppError from "./AppError"
-
-export default class NotImplementedError extends AppError {
-	constructor(message: string = "") {
-		super(500, message)
-		this.name = "NotImplemented"
-	}
-}
