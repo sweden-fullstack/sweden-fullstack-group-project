cf5758f50f648953dfbc62f011c2320b59baa0a9
d-najd <dimitar.hristijan.najdovski@gmail.com>
2026-05-16 19:27:56 +0200

    added vaidation to update in user controller update

--- backend/src/modules/user/user.controller.ts
+++ backend/src/modules/user/user.controller.ts
@@ -3,6 +3,7 @@ import userService from "./user.service"
 import UserUpdate from "@/shared/types/user/user.update"
 import typia from "typia"
 import JwtPayloadExtended from "@/shared/types/jwt/jwtPayloadExtended"
+import ForbiddenError from "@/errors/ForbiddenError"
 
 class UserController {
 	async getAll(_req: Request, res: Response) {
@@ -18,8 +19,13 @@ class UserController {
 	}
 
 	async update(req: Request, res: Response) {
+		const jwt = req.user as JwtPayloadExtended
 		const id = parseInt(req.params.id as string)
 		const body = typia.assertEquals<UserUpdate>(req.body)
+
+		if (jwt.userId !== id && jwt.userRole !== "admin") {
+			throw new ForbiddenError()
+		}
 
 		const newUser = await userService.update(id, body)
 		return res.status(200).json(newUser)
