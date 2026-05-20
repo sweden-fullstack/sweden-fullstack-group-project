303aec2012b6e5357eaf890d77eced2b65d329a8
d-najd <dimitar.najdovskiw@gmail.com>
2026-05-20 16:10:36 +0200

    revert changes

--- backend/src/modules/user/user.controller.ts
+++ backend/src/modules/user/user.controller.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express"
 import userService from "./user.service"
 import UserUpdate from "@/shared/types/user/user.update"
-import UserCreate from "@/shared/types/user/user.create"
 import typia from "typia"
 import JwtPayloadExtended from "@/shared/types/jwt/jwtPayloadExtended"
 import ForbiddenError from "@/errors/ForbiddenError"
@@ -10,20 +9,6 @@ class UserController {
 	async getAll(_req: Request, res: Response) {
 		const users = await userService.getAll()
 		res.json(users)
-	}
-
-	async getById(req: Request, res: Response) {
-		const id = parseInt(req.params.id as string)
-
-		const user = await userService.getById(id)
-		res.json(user)
-	}
-
-	async create(req: Request, res: Response) {
-		const body = typia.assertEquals<UserCreate>(req.body)
-		const newUser = await userService.create(body)
-
-		res.status(201).json(newUser)
 	}
 
 	async update(req: Request, res: Response) {
